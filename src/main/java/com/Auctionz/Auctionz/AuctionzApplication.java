31257f939d54a9dfe096ebaca8128c867505d48d
Divakarsbhat01 <divakarsbhat@gmail.com>
2024-08-11 11:56:43 +0530

    Created Basic Structure

---
+++ src/main/java/com/Auctionz/Auctionz/AuctionzApplication.java
@@ -0,0 +1,13 @@
+package com.Auctionz.Auctionz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Auctionz/Auctionz/AuctionzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auctionz.Auctionz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
